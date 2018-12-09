2046eb23a653090ce595dae7a1f5de736cb00e95
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-12-09 22:00:32 +0300

    Added script for execute database migration by add new column (normalized contact phone number)

---
+++ alembic/versions/ece6c9ebeb97_add_normalized_contact_phone_column.py
@@ -0,0 +1,27 @@
+"""add normalized contact phone column
+
+Revision ID: ece6c9ebeb97
+Revises: 
+Create Date: 2018-12-09 21:46:17.286164
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ece6c9ebeb97'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        'orders',
+        sa.Column('contact_phone_normalized', sa.String(100)),
+    )
+
+
+def downgrade():
+    op.drop_column('orders', 'contact_phone_normalized')
