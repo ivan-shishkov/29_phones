53bf7d665d6fdb0ab95eab49ce5d0c5b2d4969df
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-12-26 13:18:20 +0300

    Added timeout between transactions to reduce database load

--- normalize_phones.py
+++ normalize_phones.py
@@ -6,6 +6,7 @@ from db import db_session, Order
 
 TIMEOUT_BETWEEN_PHONES_NORMALIZATION_CYCLES = 5 * 60
 TIMEOUT_WHEN_ERROR = 10
+TIMEOUT_BETWEEN_TRANSACTIONS = 5
 
 
 def get_normalized_phone_number(source_phone_number, count_digits=10):
@@ -35,6 +36,8 @@ def run_phones_normalization_cycle(count_rows_per_transaction=100):
             break
 
         normalize_contact_phones(orders)
+
+        time.sleep(TIMEOUT_BETWEEN_TRANSACTIONS)
 
 
 def main():
